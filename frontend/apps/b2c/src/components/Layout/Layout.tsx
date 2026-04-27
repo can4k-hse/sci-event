4c4740e226850e07116d9482ce0c73a165c02f7d
Alexandr Yakovlev <aaiakovlev@edu.hse.ru>
2026-04-28 00:52:37 +0300

    FRONT-7: query param интеграция для шторки EventRegister, Layout с роутингом, fix @types/react до v19 (#21)

---
+++ frontend/apps/b2c/src/components/Layout/Layout.tsx
@@ -0,0 +1,34 @@
+import { Outlet, useSearchParams } from 'react-router-dom';
+import { BottomSheet } from '@sci-event/ui';
+import { EventRegister } from '../../pages/EventRegister';
+import { event, places } from '../../mocks/event';
+import { speakers } from '../../mocks/speakers';
+import styles from './Layout.module.css';
+
+const Layout = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const isOpen = searchParams.has('register');
+
+  const handleClose = () =>
+    setSearchParams(p => { p.delete('register'); return p; });
+
+  return (
+    <div className={styles.layout}>
+      <main className={styles.main}>
+        <Outlet />
+      </main>
+
+      <BottomSheet open={isOpen} onClose={handleClose}>
+        <EventRegister
+          event={event}
+          speakers={speakers}
+          place={places.find(p => p.place_id === event.place_id)!}
+          onRegister={handleClose}
+          onClose={handleClose}
+        />
+      </BottomSheet>
+    </div>
+  );
+};
+
+export { Layout };
--- frontend/apps/b2c/src/main.tsx
+++ frontend/apps/b2c/src/main.tsx
@@ -1,10 +1,18 @@
 import './index.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from './components/Layout';
 import App from './App';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="*" element={<App />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
