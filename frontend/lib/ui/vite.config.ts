c4b24dc2f70624377e7fbc804888ff32e1bd927f
Alexandr Iakovlev <aaiakovlev@edu.hse.ru>
2026-05-04 15:26:08 +0300

    FRONTFRAMEWORK-12: чиним сборку lib/ui

---
+++ frontend/lib/ui/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    dts({
+      include: ['src'],
+      exclude: ['src/**/*.test.*'],
+      rollupTypes: true,
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      formats: ['es'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react/jsx-runtime', 'react-dom'],
+      output: {
+        assetFileNames: '[name][extname]',
+      },
+    },
+    sourcemap: true,
+  },
+});
